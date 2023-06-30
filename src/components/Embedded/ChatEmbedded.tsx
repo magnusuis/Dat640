2087fde6e78b1d9cdeba3fa1a4622099a45c4f19
IKostric <ivica_kostric@yahoo.com>
2023-06-30 03:01:55 +0200

    Remove page dependant style

--- src/components/Embedded/ChatEmbedded.tsx
+++ src/components/Embedded/ChatEmbedded.tsx
@@ -1,24 +1,15 @@
 import "./ChatEmbedded.css";
 import React from "react";
 import ChatBox from "../ChatBox/ChatBox";
-import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
 import { Config } from "../../types";
 
 export default function ChatEmbedded(props: Config) {
   return (
-    <MDBContainer className="py-5">
-      <MDBRow className="d-flex justify-content-center">
-        <MDBCol md="10" lg="8" xl="6">
-          <div className="chat-widget-content">
-            <ChatBox
-              name={props.name || "Chatbot"}
-              use_feedback={props.useFeedback || false}
-              serverUrl={props.serverUrl}
-              socketioPath={props.socketioPath}
-            />
-          </div>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <ChatBox
+      name={props.name || "Chatbot"}
+      use_feedback={props.useFeedback || false}
+      serverUrl={props.serverUrl}
+      socketioPath={props.socketioPath}
+    />
   );
 }
